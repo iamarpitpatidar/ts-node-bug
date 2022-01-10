a4a2b995532591cf05b9752d5bcd7d584f5e429a
Arpit Patidar <arpit.ptdr@hotmail.com>
2022-01-10 13:36:07 +0530

    minimal

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,1 @@
-process.on('exit', (code) => {
-  client.logger.info(`NodeJS process exited with code ${code}`)
-})
-
-process.on('uncaughtException', (err) => {
-  client.logger.error(`UNCAUGHT_EXCEPTION: ${err}`)
-  client.logger.warn('Uncaught Exception detected, trying to restart...')
-  process.exit(1)
-})
-process.on('unhandledRejection', (reason) => {
-  client.logger.error(
-    `UNHANDLED_REJECTION: ${
-      (reason as Error).stack ? reason : new NoStackError(reason as string)
-    }`
-  )
-})
-process.on('warning', client.logger.warn)
+console.log('Hello')
